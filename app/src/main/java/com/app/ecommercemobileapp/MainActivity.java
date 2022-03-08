1c24e84a591fd3bad47e3fe9bd3129527d7b0b3f
Asad <asadul201586@gmail.com>
2022-03-08 19:21:48 +0600

    firebase added

--- app/src/main/java/com/app/ecommercemobileapp/MainActivity.java
+++ app/src/main/java/com/app/ecommercemobileapp/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference rootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initValue();
+
+
+
+    }
+
+    private void initValue() {
+        rootRef = FirebaseDatabase.getInstance().getReference();
     }
 }
